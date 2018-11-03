ab6bd7251aab49b07a077baa28d596bb900a6564
Matti Lankinen <m.lankinen@iki.fi>
2018-11-04 00:50:22 +0200

    Add Clojars release scripts

--- project.clj
+++ project.clj
@@ -5,10 +5,13 @@
   :license {:name "MIT"
             :url  "https://opensource.org/licenses/MIT"}
   :plugins [[lein-ancient "0.6.15"]]
+  :auto-clean false
   :dependencies [[net.java.dev.jna/jna "5.0.0"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :resource-paths ["resources"]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]]}}
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.9.0"]
+                                  [commons-io "2.6"]]}
+             :jar {:dependencies [[org.clojure/clojure "1.9.0"]]}}
   :aliases {"t" ["test"]}
   :release-tasks [["deploy"]])
